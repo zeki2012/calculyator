46ddf76b409caf92070e7fe516edc7d7ec36c18e
= <=>
2024-02-03 08:45:22 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let netice;
+
+topla.onclick = () => {
+    netice = `${rqm1.value} + ${rqm2.value} = ${rqm1.value + rqm2.value}`
+    document.getElementById('hesab').innerHTML = netice
+}
+cix.onclick = () => {
+    netice = `${rqm1.value} - ${rqm2.value} = ${rqm1.value - rqm2.value}`
+    document.getElementById('hesab').innerHTML = netice
+}
+
+vur.onclick = () => {
+    netice = `${rqm1.value} * ${rqm2.value} = ${rqm1.value * rqm2.value}`
+    document.getElementById('hesab').innerHTML = netice
+}
+
+bol.onclick = () => {
+    netice = `${rqm1.value} / ${rqm2.value} = ${rqm1.value / rqm2.value}`
+    document.getElementById('hesab').innerHTML = netice
+}
